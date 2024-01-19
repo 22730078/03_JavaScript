94e7bd99e8c380481cb5fbd0530bb21dff2aaaeb
Young <kelly7893@naver.com>
2024-01-19 13:51:00 +0900

    20240119_JS수업내용

--- js/14_입력한색으로변경.js
+++ js/14_입력한색으로변경.js
@@ -1,18 +1,12 @@
-const box = document.querySelector("#box");
-const input = document.querySelector("#input");
-const btn = document.querySelector("#btn");
+//요소를 한번에 여러개 얻어오면 배열 형태로 저장된다 -> 얻어온 요소를 사용하려면 배열의 index별 요소를 하나씩 꺼내서 사용해야 한다
+// ex) boxList.style.color="red"     = 배열의 글자색을  red로 변경 (x)
+// ->  boxList[0].style.color="blue" = 0번 인덱스 요소 글자색 blue (o)
 
-//#btn1 클릭했을 때
-btn.addEventListener("click", function(){
+const boxList = document.querySelectorAll(".box");
+const inputList = document.querySelectorAll(".input");
 
-    box.style.backgroundColor = input.value 
-});
-
-//#input1에서 엔터 입력(key 관련 이벤트) 시 배경색 변경
-input.addEventListener("keyup", function(e){
-
-   if(e.key == "Enter"){
-
-    box.style.backgroundColor = input.value;
-  }
+document.querySelector("#change").addEventListener("click",function(){
+    for(let i=0; i<boxList.length; i++){
+      boxList[i].style.backgroundColor = inputList[i].value;
+    }
 });
